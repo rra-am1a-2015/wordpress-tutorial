c7292df43a33a8a6e0132650a72d4d12adb6103f
Arjan de Ruijter <rra@mboutrecht.nl>
2016-06-02 11:04:42 +0200

    Wegschrijven naar database

---
+++ wp-content/plugins/login-form/login-form.php
@@ -0,0 +1,66 @@
+<?php
+    /*
+        Plugin Name: Login Form
+        Plugin URI: localhost/am1a/index.php?content=login
+        Version: 1.0
+        Author: Arjan de Ruijter  
+        Author URI: localhost/am1a
+        Description: Dit is een login form plugin. 
+        License:     GPL2 
+        {Plugin Name} is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 2 of the License, or
+        any later version.
+        
+        {Plugin Name} is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+        GNU General Public License for more details.
+        
+        You should have received a copy of the GNU General Public License
+        along with {Plugin Name}. If not, see {License URI}.   
+        License URI: https://www.gnu.org/licenses/gpl-2.0.html 
+    */
+    
+    
+    function login_form_shortcode()
+    {
+        
+        //var_dump($_POST);
+        if (isset($_POST["submit"]))
+        {
+            date_default_timezone_set("Europe/Amsterdam");
+            $date = date("Y-m-d H:i:s");
+            echo $date;
+            global $wpdb;
+        
+            $wpdb->insert("wp_users", array(
+                "user_login" => $_POST["firstname"],
+                "user_registered" => $date         
+            ));
+            
+            $output = "<p>Mijn naam is: ".$_POST["firstname"]." "
+                                         .$_POST["infix"]." "
+                                         .$_POST["lastname"]."</p>";
+        }
+        
+        $output .= "<form method='post' action='http://localhost/voorlichtingsavondmboutrecht/index.php/loginform/'>
+                        voornaam: <input type='text' name='firstname' >
+                        tussenvoegsel: <input type='text' name='infix' >
+                        achternaam: <input type='text' name='lastname'>
+                        <input type='submit' name='submit'>";               
+        return $output;
+    
+    }
+    
+    function login_form_register_shortcode()
+    {
+        add_shortcode('login-form', 'login_form_shortcode');
+    }
+    
+    add_action('init', 'login_form_register_shortcode');
+    
+    /*
+            [hallo_wereld  firstname="Arjan" infix="de" lastname="Ruijter" picture="panda1.jpg"]
+    */
+?>
